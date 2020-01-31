0891f606d3164948e04bb97039c42677b7fd5b56
Maas Lalani <maaslalani1@gmail.com>
2020-01-31 14:34:46 -0500

    seo

---
+++ build/precache-manifest.54367ca96373c7aee3ad0e0f5bf88769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cefcfa7a3593810e153064929a4e9d65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd5678f0db2ce906cc8d",
+    "url": "/static/css/main.a782359d.chunk.css"
+  },
+  {
+    "revision": "bc15a9846d5744a95a5d",
+    "url": "/static/js/2.c1e5f6fa.chunk.js"
+  },
+  {
+    "revision": "cd5678f0db2ce906cc8d",
+    "url": "/static/js/main.fa36cef7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
