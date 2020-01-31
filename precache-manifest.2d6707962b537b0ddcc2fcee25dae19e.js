27b0fecf3cf2fbea255d3a39c7df8acd9bb0a28d
Adam Gonen <adam.gonen123@gmail.com>
2020-01-31 14:35:49 -0500

    Updates

---
+++ precache-manifest.2d6707962b537b0ddcc2fcee25dae19e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef73e0c206a0adbca9bc890a324e12c",
+    "url": "/maaslalani/OffsetList/index.html"
+  },
+  {
+    "revision": "606399369a6f86a703db",
+    "url": "/maaslalani/OffsetList/static/css/main.a782359d.chunk.css"
+  },
+  {
+    "revision": "bc15a9846d5744a95a5d",
+    "url": "/maaslalani/OffsetList/static/js/2.c1e5f6fa.chunk.js"
+  },
+  {
+    "revision": "606399369a6f86a703db",
+    "url": "/maaslalani/OffsetList/static/js/main.9e3c9033.chunk.js"
+  },
+  {
+    "revision": "4f6ab0fed8fbe3e15ab8",
+    "url": "/maaslalani/OffsetList/static/js/runtime~main.e4adaf2b.js"
+  }
+]);
