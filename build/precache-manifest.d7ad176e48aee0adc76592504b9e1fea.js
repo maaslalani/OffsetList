7ec36d1292ac0fc2d7533dc4f6835f97d0ee44d4
Maas Lalani <maaslalani1@gmail.com>
2020-01-31 15:16:42 -0500

    daw

--- build/precache-manifest.d7ad176e48aee0adc76592504b9e1fea.js
+++ build/precache-manifest.d7ad176e48aee0adc76592504b9e1fea.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "59c5572e84b009503be1dfae51035a87",
+    "revision": "a8bf054dbcaa86767d926d67d6850be9",
     "url": "/offsetlist/index.html"
   },
   {
-    "revision": "be1cb9437a9ea22870ba",
-    "url": "/offsetlist/static/css/main.a782359d.chunk.css"
+    "revision": "99efdd77771fde5d5584",
+    "url": "/offsetlist/static/css/main.786d0a20.chunk.css"
   },
   {
     "revision": "bc15a9846d5744a95a5d",
     "url": "/offsetlist/static/js/2.c1e5f6fa.chunk.js"
   },
   {
-    "revision": "be1cb9437a9ea22870ba",
+    "revision": "99efdd77771fde5d5584",
     "url": "/offsetlist/static/js/main.5d366f3c.chunk.js"
   },
   {
